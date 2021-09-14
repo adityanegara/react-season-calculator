4dfb4c0c70cbda8e88ab2fc28754a6900b403a0d
adityanegara <aditnegarabisnis@gmail.com>
2021-09-14 15:44:30 +0700

    Finish Getting current lat, long, month, and season

--- src/App.js
+++ src/App.js
@@ -3,13 +3,43 @@ import SeasonDisplay from "./Components/SeasonDisplay";
 import Title from "./Components/Title";
 import './Styles/main.css';
 class App extends React.Component{
+    state= {latitude: null, longitude: null, errorMessage:'', }
+    componentDidMount(){
+        this._getUserLocation();
+    }
     render = () =>{
-        return (
-            <React.Fragment>
-                <Title title="SEAS CALC"/>
-                <SeasonDisplay/>
-            </React.Fragment>
-       
+        if(this.state.errorMessage === '' && this.state.latitude != null){
+            return (
+                <React.Fragment>
+                    <Title title="SEAS CALC"/>
+                    <SeasonDisplay latitude={this.state.latitude} longitude={this.state.longitude} month={new Date().getMonth()}/>
+                </React.Fragment>
+            )
+        }
+        if(this.state.errorMessage && this.state.latitude == null){
+            return (
+                <React.Fragment>
+                    <Title title={this.state.errorMessage}/>
+                </React.Fragment>
+            )
+        }else{
+            return (
+                <React.Fragment>
+                    <Title title='Load'/>
+                </React.Fragment>
+            )
+        }
+     
+    }
+    _getUserLocation(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ latitude: position.coords.latitude })
+                this.setState({longitude:position.coords.longitude})
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message })
+            }
         )
     }
 }
--- src/Components/SeasonDisplay.js
+++ src/Components/SeasonDisplay.js
@@ -4,7 +4,7 @@ class SeasonDisplay extends React.Component{
     render = () =>{
         return(
             <div className="season-display">
-                <div className="season-display-description">Brrr Its Cold!</div>
+                <div className="season-display-description">{this._displayDescription(this._getSeason(this.props.latitude, this.props.month))}</div>
                 <div className="season-display-input">
                     <div className="season-display-input-form">
                         <label htmlFor="input-latitude">Latitude</label>
@@ -43,25 +43,76 @@ class SeasonDisplay extends React.Component{
                 <div className="season-display-information">
                     <div className="flex">
                        <p>Latitude  </p>
-                       <p className="bigger">138.5</p>
+                       <p className="bigger">{this.props.latitude}</p>
                     </div>
                     <div className="flex">
                        <p>Longitude  </p>
-                       <p className="bigger">138.5</p>
+                       <p className="bigger">{this.props.longitude}</p>
                     </div>
                     <div className="flex">
                        <p>Month  </p>
-                       <p className="bigger">January</p>
+                       <p className="bigger">{this._convertMonth(this.props.month)}</p>
                     </div>
                     <div className="flex">
                        <p>Season  </p>
-                       <p className="bigger">Winter</p>
+                       <p className="bigger">{this._getSeason(this.props.latitude, this.props.month)}</p>
                     </div>
                    
                 </div>
             </div>
         )
     }
+    _convertMonth(month){
+        switch(month) {
+            case 0:
+              return 'January';
+            case 1:
+              return 'Feburary';
+            case 2:
+              return 'March';
+            case 3:
+              return 'April';
+            case 4:
+              return 'May';
+            case 5:
+              return 'June';
+            case 6:
+              return 'July';
+            case 7:
+              return 'August';
+            case 8:
+              return 'September';
+            case 9:
+              return 'October';
+            case 10:
+              return 'November'
+            default:
+              return 'December';
+          }
+    }
+
+    _getSeason = (latitude, month) =>{ 
+        if(month > 2 && month < 9){
+            return latitude > 0 ? 'Summer' : 'Winter';
+        }else{
+            return latitude > 0 ? 'Winter' : 'Summber';
+        }
+    }
+
+    _displayDescription = (season) =>{
+        if(season === 'Summer'){
+            return 'Huft Its Hot!';
+        }else{
+            return 'Brr Its Cold!';
+        }
+    }
+    
+}
+
+SeasonDisplay.defaultProps = {
+    latitude: '0',
+    longitude: '0',
+    month:'January',
 }
 
 export default SeasonDisplay;
